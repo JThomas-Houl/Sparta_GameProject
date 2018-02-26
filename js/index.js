942d8b611fd21153852c322f535a4359662c3d2a
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-02-26 11:08:48 +0000

    you can win and lose. lose bug: even if you on the finish square you can lose

--- js/index.js
+++ js/index.js
@@ -2,22 +2,42 @@ $(function (event){
 	console.log("DOM is ready");
 
 	var pixelGrid = $(".pixelGrid"); //pixel grid variable
+	var startBlock = $(".E_col_1");
+	var finishBlock = $(".E_col_10");
+	var mouseIsUp = true
 
 	//on mouse click event
-	pixelGrid.mousedown(function(event){
-		console.log("down");
+	startBlock.mousedown(function(event){
+	//lets you only click on startblock once
+		//console.log("startBlock");
 
 		//mousemove event on the td uses this to paint the color of the cell
 		$('td').mousemove(function(event){
 			$(this).css("background-color", "red");
 
-			});
+		});
+
 		//mouse up funtion turns off all events once the mouse is up
+		$(finishBlock).mouseup(function(event){
+			$('td').off();
+			console.log("you win");
+		});
+
 		$(pixelGrid).mouseup(function(event){
+			console.log("you lose ");
 			$('td').off();
 		});
 
-		});
-		
+
 
 	});
+
+	// startBlock.click(function(event){
+	// 	console.log("start");
+	// });
+
+
+
+		
+
+});
