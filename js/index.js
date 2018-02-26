55865f63b3f8a09677f7b24d13060be091e06f33
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-02-26 12:04:58 +0000

    you can win and lose and paint colors

--- js/index.js
+++ js/index.js
@@ -12,29 +12,23 @@ $(function (event){
 		//mousemove event on the td uses this to paint the color of the cell
 		$('td').mousemove(function(event){
 			$(this).css("background-color", "red");
-			//console.log($(this).html());
+			
 
 		});
 
-		// //mouse up win funtion turns off all events once the mouse is up
-		// $(finishBlock).mouseup(function(event){
-		// 	$('td').off();
-		// 	console.log("you win");
-		// });
+		// //mouse up on td
+		$('td').mouseup(function(event){
 
-		
-
-
-		// //mouse up you lose
-		$(pixelGrid).mouseup(function(event){
-
-			if($(this)!= finishBlock){
-				console.log("you lose");
+			/*checks if the html = finishblock if yes you win
+			if the inner html is not the same then you lose*/
+				
+			if($(this).html() !== finishBlock.html()){
 				$('td').off();
+				console.log("you lose");
 
-			}else if($(this) == finishBlock){
+			}else{
 				console.log("you win");
-			
+				$('td').off();
 			}
 
 		});
@@ -42,16 +36,6 @@ $(function (event){
 
 
 	});
-
-	//if mouse up not on finihs block then you lose 
-	/* if the mouse comes up on a cell thats not finishBlock
-	then you lose.
-
-	check if its the finishBlock on mouse up*/
-
-	
-
-
 		
 
 });
