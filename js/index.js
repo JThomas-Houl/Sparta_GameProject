cf2ada298f405b9e6aa3d195bd1c1b860cf7daec
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-02-26 16:25:23 +0000

    collsions nearly working

--- js/index.js
+++ js/index.js
@@ -4,15 +4,47 @@ $(function (event){
 	var pixelGrid = $(".pixelGrid"); //pixel grid variable
 	var startBlock = $(".E_col_1");
 	var finishBlock = $(".E_col_10");
+
+	var deathColor = $(".row_A").css("background-color");
 	
+	//div p
+	// var topDivBBPos = $(".topDivBB").position();
+	// var middleDivBBPos = $(".middleDivBB").position();
+	// var bottomLeftDivBBPos = $(".bottomLeftDivBB").position();
+	// var bottomRightDivBBPos = $(".bottomRightDivBB").position();
+	// var bottomMiddleDivBBPos = $(".bottomMiddleDivBB" ).position();
 
 	//on mouse click event you can only click on startblock once
 	startBlock.mousedown(function(event){
+	// console.log(event.pageX,event.pageY);
+	// console.log(topDivBBPos.left, topDivBBPos.top);
+
+		
+		//console.log($(".A_col_1").css("background-color"));
+
 
 		//mousemove event on the td uses this to paint the color of the cell
 		$('td').mousemove(function(event){
-			$(this).css("background-color", "red");
-			
+
+
+			//$(this).css("background-color", "red");
+
+			//console.log($(this).css("background-color"));
+
+
+			if($(this).css("background-color") !== deathColor){
+				console.log("dead");
+			}
+
+
+
+			//console.log(event.pageX, event.pageY);
+			// console.log($("topDivBB").pageX);
+
+
+			// console.log(event.pageX, event.pageY);
+
+
 
 		});
 
@@ -24,10 +56,10 @@ $(function (event){
 
 			if($(this).html() !== finishBlock.html()){
 				$('td').off();
-				alert("you lose");
+				//alert("you lose");
 
 			}else{
-				alert("you win");
+				//alert("you win");
 				$('td').off();
 			}
 
