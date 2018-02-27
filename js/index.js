9041d7ba3f58e066393c5c6e7b65022c319bfab1
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-02-27 14:54:38 +0000

    level change broken

--- js/index.js
+++ js/index.js
@@ -18,6 +18,18 @@ $(function (event){
 
 	var mouseIsUp = false;
 
+	var secondLevelGrid = [$(".A_col_5"), $(".A_col_10"), $(".B_col_5"), $(".B_col_8"), $(".B_col_10"),$(".C_col_2"), 
+							$(".C_col_5"),$(".C_col_8"),$(".C_col_10"), $(".D_col_1"), $(".D_col_3"), $(".D_col_8"),
+							$(".E_col_1"),$(".E_col_4"), $(".E_col_5"),$(".E_col_7"),$(".E_col_8"),$(".E_col_9"),
+							$(".F_col_6"),$(".G_col_1"),$(".G_col_4"),$(".G_col_6"),$(".G_col_9"),$(".G_col_10"),
+							$(".H_col_6"),$(".I_col_2"),$(".I_col_3"),$(".I_col_5"),$(".I_col_6"),$(".I_col_10"),
+							$(".J_col_5"),$(".J_col_6"),$(".J_col_10")];
+	
+	var secondLevelArray = secondLevelGrid.length;
+
+	var startBlockLevel2 = $("H_col_10");
+
+	var finishBlockLevel2 = $("F_col_1");
 
 	function resetGrid(){
 		for(var i = 0; i < tableSize; i++){ //reads table size
@@ -36,17 +48,33 @@ $(function (event){
 		}
 	}
 
+	function gridWipeNewLevel(){
+		for(var i = 0; i < tableSize; i++){ //reads table size
+			var tableCellColorInteration = $($("td")[i]).css("background-color"); //td array colors
+			var tableInteration = $($("td")[i]); //td interation
+			tableInteration.css("background-color", "white");
+		}
+		
+	}
+
+	function printNewLev(){
+		for(var i = 0; i < secondLevelArray; i++){
+			console.log($(secondLevelGrid[i]));
+		}
+	}
 	// do{
 
 	//on mouse click event you can only click on startblock once
 	startBlock.mousedown(function(event){
 		mouseIsUp = false;
+		printNewLev();
 
 		//mousemove event on the td uses this to paint the color of the cell
 		$('td').mousemove(function(event){
 
 			if(mouseIsUp == false) {
 				if($(this).css("background-color") === deathColor){
+
 					console.log("dead");
 					//$('td').off();
 					//alert("you lose");
@@ -82,26 +110,32 @@ $(function (event){
 
 		}
 
+
+		var userInput = prompt("y for agian");
+		if (userInput == "y"){
+			mouseIsUp == false;
+		 }
+
 		else{
 			alert("you win");
-			$('td').off();
+			//$('td').off();
+
+			// var nextLevel = prompt("y for next Level");
+			// if(nextLevel == "y"){
+			//  	mouseIsUp == false;
+			//  	
+			// }
 		}
 
-		// var userInput = prompt("y for agian");
-		// if (userInput == "y"){
-		// 	mouseIsUp == false;
-		// }
+		
+
+
+
+
 
 	});
 	
-
-
-		function resetMouse(){
-			pixelGrid.mouseup(function(){
-				console.log("mouse up");
-				
-			});
-		}
+//level change:
 
 	// }while(gameStart == false);
 	
