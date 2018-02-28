5bc22e5e31db44c5813ee6202811fa85572927ec
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-02-28 16:55:44 +0000

    making level 3 still need to link its all up

--- js/index.js
+++ js/index.js
@@ -30,15 +30,6 @@ $(".purple").css("background-color", "purple");
 
 	var mouseIsUp = false;
 
-	var secondLevelGrid = [$(".A_col_5"), $(".A_col_10"), $(".B_col_5"), $(".B_col_8"), $(".B_col_10"),$(".C_col_2"), 
-							$(".C_col_5"),$(".C_col_8"),$(".C_col_10"), $(".D_col_1"), $(".D_col_3"), $(".D_col_8"),
-							$(".E_col_1"),$(".E_col_4"), $(".E_col_5"),$(".E_col_7"),$(".E_col_8"),$(".E_col_9"),
-							$(".F_col_6"),$(".G_col_1"),$(".G_col_4"),$(".G_col_6"),$(".G_col_9"),$(".G_col_10"),
-							$(".H_col_6"),$(".I_col_2"),$(".I_col_3"),$(".I_col_5"),$(".I_col_6"),$(".I_col_10"),
-							$(".J_col_5"),$(".J_col_6"),$(".J_col_10")];
-	
-	var secondLevelArray = secondLevelGrid.length;
-
 	var startBlockLevel2 = $("H_col_10");
 
 	var finishBlockLevel2 = $("F_col_1");
@@ -80,7 +71,7 @@ $(".purple").css("background-color", "purple");
 	______________________________________________________________________________ */
 
 	//on mouse click event you can only click on startblock once
-	startBlock.mousedown(function(event){
+	$(".StartBlock").mousedown(function(event){
 		mouseIsUp = false;
 		
 		
@@ -89,7 +80,7 @@ $(".purple").css("background-color", "purple");
 		$('td').mousemove(function(event){
 
 			if(mouseIsUp == false) {
-				if($(this).css("background-color") === deathColor){
+				if($(this).css("background-color") === $(".Green").css("background-color")){
 
 					console.log("dead");
 					//$('td').off();
@@ -116,84 +107,72 @@ $(".purple").css("background-color", "purple");
 		/*checks if the html = finishblock if yes you win
 		if the inner html is not the same then you lose*/
 
-		if($(this).html() !== finishBlock.html()){
+		if($(this).html() !== $(".FinishBlock").html()){
 			//$('td').off();
 			console.log("you lose");
 			//location.reload();
 			resetGrid();
-			
+		}
+		else if($(".White").css("background-color") === $(".FinishBlock").css("background-color")){
+			alert("you win");
+		}
 		var userInput = prompt("y to retry, N to not");
 		if (userInput == "y"){
 			mouseIsUp == false;
 		 }
 
-
-		}
-
-
-
-		else{
-			alert("you win");
-			//$('td').off();
-
-			// var nextLevel = prompt("y for next Level");
-			// if(nextLevel == "y"){
-			//  	mouseIsUp == false;
-			//  	
-			// }
-		}
 	});
 	
 
-	startBlockV2.mousedown(function(event){
-		mouseIsUp = false;
-			//mousemove event on the td uses this to paint the color of the cell
-			$('td').mousemove(function(event){
+	// startBlockV2.mousedown(function(event){
+	// 	mouseIsUp = false;
+	// 		//mousemove event on the td uses this to paint the color of the cell
+	// 		$('td').mousemove(function(event){
 
-				if(mouseIsUp == false) {
-					if($(this).css("background-color") === deathColorV2){
+	// 			if(mouseIsUp == false) {
+	// 				if($(this).css("background-color") === $(".Green").css("background-color")){
 
-						console.log("dead");
-						//$('td').off();
-						//alert("you lose");
-						//location.reload(); page reset
-						resetGrid();
+	// 					console.log("dead");
+	// 					//$('td').off();
+	// 					//alert("you lose");
+	// 					//location.reload(); page reset
+	// 					resetGrid();
 						
-					}else {
-							$(this).css("background-color", "red");
-					}
-				}
+	// 				}else {
+	// 						$(this).css("background-color", "red");
+	// 				}
+	// 			}
 
-			});
+	// 		});
 
-			// //mouse up on td
-	});
-
-
-
-	$('td').mouseup(function(event){
-		mouseIsUp = true;
+	// 		// //mouse up on td
+	// });
 
 
-		/*checks if the html = finishblock if yes you win
-		if the inner html is not the same then you lose*/
 
-		if($(this).html() !== finishBlockV2.html()){
-			//$('td').off();
-			console.log("you lose");
-			//location.reload();
-			resetGrid();
+	// $('td').mouseup(function(event){
+	// 	mouseIsUp = true;
+
+
+	// 	/*checks if the html = finishblock if yes you win
+	// 	if the inner html is not the same then you lose*/
+
+	// 	if($(this).html() !== $(".FinishBlock")){
+	// 		//$('td').off();
+	// 		console.log("you lose");
+	// 		//location.reload();
+	// 		resetGrid();
 			
-		var userInput = prompt("y to retry, N to not");
-		if (userInput == "y"){
-			mouseIsUp == false;
-		 }
+	// 	var userInput = prompt("y to retry, N to not");
+	// 	if (userInput == "y"){
+	// 		mouseIsUp == false;
+	// 	 }
 
 
-		}
-		else{
-			alert("you win");
-		}
-	});
+	// 	}
+	// 	else{
+	// 		alert("you win");
+	// 	}
+	// });
 
 });
